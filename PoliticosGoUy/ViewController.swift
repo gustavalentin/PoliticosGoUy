c18896efce1d79af0198b9f5cded8fe08bbb1403
Gustavo Rodriguez Ramos <Gustavo@macbook-pro-de-gustavo.local>
2016-10-11 20:54:12 -0300

    Initial Commit

---
+++ PoliticosGoUy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoliticosGoUy
+//
+//  Created by Gustavo Rodriguez Ramos on 10/11/16.
+//  Copyright © 2016 Gustavo Rodriguez Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
